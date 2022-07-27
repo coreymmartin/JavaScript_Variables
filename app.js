4fc10ce5bc3eedc94a9c052352e2188cf33bd7e2
corey <coreymmartin1991@gmail.com>
2022-07-27 14:57:23 -0500

    exercises completed

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
 // Exercise 2
-
 // Exercise 3
+
+const firstName = "Corey";
+let lastName = "Martin";
+var age = 31;
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`
+hello ${firstName} ${lastName}, for your ${age + 1} birthday you have won  a book! the book is: ${title} by ${author} @ ${pageCount} pages. enjoy!
+`);
